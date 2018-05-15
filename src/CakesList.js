652d65abc6201c6ea2ab4ab44619b7ecd2f92289
Islam Dudaev <islamwolf23@gmail.com>
2018-05-15 12:06:46 +0100

    Add CakesList

---
+++ src/CakesList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CakesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <span>Cake list is working!</span>;
+  }
+}
+
+export default CakesList;
